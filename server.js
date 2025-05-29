f6ac3885d4c57aaa44f99e0fe2712cbf70d48a1a
DYuHLin <109787948+DYuHLin@users.noreply.github.com>
2025-05-29 16:05:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(session({secret: 'cats', cookie: {
    secure: "true",
    sameSite: "none",
-   maxAge: 30 * 24 * 60 * 60 * 1000,
+   maxAge: 1000 * 60 * 60,
   }, resave: false, saveUninitialized: false
 }))
 
